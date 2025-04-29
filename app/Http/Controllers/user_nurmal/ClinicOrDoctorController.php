eeda15e7ce3a6a64440b4e40f738f2867157c3bd
Oussama <oussamalajdelsid@email.com>
2025-04-29 07:29:53 -0700

    2025/04/29 12:19

--- app/Http/Controllers/user_nurmal/ClinicOrDoctorController.php
+++ app/Http/Controllers/user_nurmal/ClinicOrDoctorController.php
@@ -50,7 +50,7 @@ class ClinicOrDoctorController extends Controller
                     'email' => $doctor->email,
                     'phone' => $doctor->phone,
                     'specialty_name' => $doctor->specialty->name ?? null,
-                    'name_clinic'=> $clinic->name,
+                    'name_clinic'=> $doctor->clinic->name,
                     'profile_image' => $doctor->profile_image ? asset('storage/' . $doctor->profile_image) : null,
                     'Presence' => $doctor->Presence,
                 ];
