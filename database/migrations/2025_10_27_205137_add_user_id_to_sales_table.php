3d0065ad31b511bcfff64406fae0664637fa88cb
AyoubZekri <ayobzekri670@email.com>
2025-10-30 09:44:45 +0100

    30/10/2025

--- database/migrations/2025_10_27_205137_add_user_id_to_sales_table.php
+++ database/migrations/2025_10_27_205137_add_user_id_to_sales_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Sales', function (Blueprint $table) {
+        Schema::table('sales', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Sales', function (Blueprint $table) {
+        Schema::table('sales', function (Blueprint $table) {
             $table->dropcolumn("user_id");
         });
     }
