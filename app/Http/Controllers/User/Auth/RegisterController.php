1f4e15b3e40df9cb7703777d5f7e6ff109fc23fe
AyoubZekri <ayobzekri670@email.com>
2025-11-17 12:10:53 +0100

    17/11/2025

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Zakat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
