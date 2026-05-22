cca4cfaba154a79cdec73238becc491d0999fcdc
Zineb-web22 <bzineb296@gmail.com>
2026-05-22 10:41:24 +0100

    2026/05/20 2:21

--- app/Models/ChargilyPayment.php
+++ app/Models/ChargilyPayment.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ChargilyPayment extends Model
 {
     use HasFactory;
-    protected $fillable = ["user_id", "status", "currency", "amount"];
+    protected $fillable = ["user_id", "status","type", "currency", "amount"];
 }
