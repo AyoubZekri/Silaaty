0b4cac5480acd978d164a71bc7b2d118dc9aeaa6
Zineb-web22 <bzineb296@gmail.com>
2026-07-30 12:13:37 +0100

    2026/07/30 12:00

--- app/Http/Controllers/User/Saller/LoginSallerController.php
+++ app/Http/Controllers/User/Saller/LoginSallerController.php
@@ -23,13 +23,10 @@ class LoginSallerController extends Controller
                 return Respons::error('بيانات غير صحيحة', 422, $validator->errors());
             }
 
-            $userCheck = \App\Models\User::where('email', $request->email)->first();
-            if ($userCheck && $userCheck->user_role != 3) {
+            $seller = Seller::with('user')->where('email', $request->email)->first();
+            if ($seller && $seller["user_role"] != 3) {
                 return Respons::error('حسابك ليس بائع', 403);
             }
-
-            $seller = Seller::with('user')->where('email', $request->email)->first();
-
             if (!$seller || !Hash::check($request->password, $seller->password)) {
                 return Respons::error('البريد الإلكتروني أو كلمة المرور غير صحيحة', 401);
             }
